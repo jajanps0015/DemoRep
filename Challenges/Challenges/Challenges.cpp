ddbee067fab28692d84cc443adeb8b73429260a4
Jajanpreet Singh <jsingh@magicleap.com>
2023-06-30 08:36:03 -0400

    update input output

--- Challenges/Challenges/Challenges.cpp
+++ Challenges/Challenges/Challenges.cpp
@@ -1,43 +1,105 @@
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <map>
-#include <array>
+#include <vector>
 
-template<typename T>
-T Add(T num1, T num2)
-{
-    return num1 + num2;
-}
 
-template<typename T, typename T2>
-class AtomicWrapper
+class Character
 {
 public:
-    T value;
-    T2 value2;
+    const char* ancestory;
+    const char* charClass;
 
-    AtomicWrapper(T _value, T2 _value2) : value(_value), value2(_value2)
-    {
-    }
+    Character()
+        : ancestory("xxx"),
+        charClass("xxx")
+    {}
+
+    Character(const char* anc, const char* cls)
+        : ancestory(anc), charClass(cls)
+    {}
 
     void Display()
     {
-        std::cout << "The values inside wrapper are value : " << value
-            << " and value2 : " << value2 << std::endl;;
+        std::cout << "Character data : Ancestory = " << ancestory
+            << " and class = " << charClass << std::endl;
     }
 };
 
 int main()
 {
-    std::array<int, 6> numbers;
-    numbers[0] = 9;
+    // just writing to file, no reading
+    std::ofstream outputFile("binaryFile.dat", std::ios::binary);
+    if (!outputFile)
+    {
+        std::cout << "Error opening file for write : ";
+        return 0;
+    }
+    char enterMore = 'y';
+    do
+    {
+        std::string anc, cls;
 
-    std::array<char, 8> alphabets;
-    alphabets[0] = 'a';
+        std::cout << "Enter ancestory : ";
+        std::cin >> anc;
 
-    AtomicWrapper<int, char> cardWrapper(3, 'e');
-    cardWrapper.Display();
+        std::cout << "Enter class : ";
+        std::cin >> cls;
 
-    AtomicWrapper<char, double> cardWrapper2('c', 7.2);
-    cardWrapper2.Display();
+        Character coutChar(anc.c_str(), cls.c_str());
+
+        outputFile.write(coutChar.ancestory, strlen(coutChar.ancestory) + 1);
+        outputFile.write(coutChar.charClass, strlen(coutChar.charClass) + 1);
+
+
+        std::cout << "Enter more : ";
+        std::cin >> enterMore;
+
+    } while (enterMore == 'y');
+
+    outputFile.close();
+
+    std::ifstream inputFile("binaryFile.dat", std::ios::binary | std::ios::in);
+    if (!inputFile)
+    {
+        std::cout << "Error opening file for read : ";
+        return 0;
+    }
+    /*inputFile.read(reinterpret_cast<char*>(&cinChar), sizeof(Character));
+    cinChar.Display();*/
+    while (!inputFile.eof())
+    {
+        std::string readAnc;
+        std::getline(inputFile, readAnc, '\0');
+        const char* anc = readAnc.c_str();
+
+        std::string readClass;
+        std::getline(inputFile, readClass, '\0');
+        const char* cls = readClass.c_str();
+
+        Character cinChar(anc, cls);
+        cinChar.Display();
+    }
+
+    inputFile.close();
+}
+
+void iodemo()
+{
+    //char ch = 'x';
+    //int j = 77;
+    //double d = 6.02;
+    //string str1 = "Kafka"; //strings without
+    //string str2 = "Proust"; // embedded spaces
+    //ofstream outfile("fdata.txt"); //create ofstream object
+
+    //outfile << ch //insert (write) data
+    //    << j
+    //    << " " //needs space between numbers
+    //    << d
+    //    << str1
+    //    << " " //needs spaces between strings
+    //    << str2;
+    //cout << "File written\n";
+    //return 0;
 }
